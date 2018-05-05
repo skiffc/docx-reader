0e3588e03afbb66220ebe1e0fb7915b8806b4ca9
skiff <skiffc@gmail.com>
2018-05-05 12:06:44 +0800

    Update table read style

--- read.py
+++ read.py
@@ -5,22 +5,30 @@ from docx.table import Table
 from docx.text.paragraph import Paragraph 
 
 
-document = Document('test.docx')
-print document.paragraphs
-
-for p in document.paragraphs:
-    print p.text
-
-for t in document.tables:
-    for c in t.row_cells(1):
-        print c.text
+document = Document('target.docx')
+#print document.paragraphs
+#
+#for p in document.paragraphs:
+#    print p.text
+#
+#for t in document.tables:
+#    for c in t.row_cells(1):
+#        print c.text
 
 for b in iter_block_items(document):
     print b
     if isinstance(b, Paragraph):
         print b.text
     elif isinstance(b, Table):
-        for c in range(0,len(b.columns)):
-            print 'Column:', c
-            for t in b.row_cells(c):
-                print t.text
+        print 'Table'
+        for row in b.rows:
+            print '  Row'
+            for cell in row.cells:
+                print '    Cell'
+                for paragraph in cell.paragraphs:
+                    print '      Text:', paragraph.text
+
+        #for c in range(0,len(b.columns)):
+        #    print 'Column:', c
+        #    for t in b.row_cells(c):
+        #        print ' ', c, t.text
